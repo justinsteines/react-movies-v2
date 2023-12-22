a7aef66b610e466f7800777eb8dfa74de24491e3
Justin Steines <30095726+justinsteines@users.noreply.github.com>
2023-12-22 00:08:49 -0600

    feat: add active link indicator to main navigation

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -3,24 +3,27 @@ import { Icon } from '@iconify/react'
 
 function MainNavigation() {
   const links = [
-    { path: '/', iconName: 'material-symbols:home-rounded' },
-    { path: '/movies', iconName: 'material-symbols:movie' },
-    { path: '/shows', iconName: 'material-symbols:tv-outline-rounded' },
+    { path: '/', iconName: 'bx:home-alt-2' },
+    { path: '/movies', iconName: 'mdi:movie-open-outline' },
+    { path: '/shows', iconName: 'bx:tv' },
   ]
 
   return (
     <nav className="border-t-2 border-slate-50/20 lg:order-first lg:border-r-2 lg:border-t-0">
       <ul className="flex h-20 items-center justify-center gap-8 lg:my-8 lg:w-24 lg:flex-col lg:items-center lg:justify-start lg:h-screen-safe">
         {links.map((link) => (
-          <NavLink
-            to={link.path}
-            key={link.path}
-            className={({ isActive }) => (isActive ? 'underline' : undefined)}
-          >
-            <li className="p-4">
-              <Icon icon={link.iconName} className="text-4xl text-sky-400" />
-            </li>
-          </NavLink>
+          <li key={link.path}>
+            <NavLink
+              to={link.path}
+              className={({ isActive }) =>
+                `${
+                  isActive ? 'text-sky-400' : 'text-slate-100'
+                } inline-block p-4 text-4xl`
+              }
+            >
+              <Icon icon={link.iconName} />
+            </NavLink>
+          </li>
         ))}
       </ul>
     </nav>
