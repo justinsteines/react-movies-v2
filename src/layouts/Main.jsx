efad8ce3d6b1d3b16f45f2ff925b89e50cc54d3a
Justin Steines <30095726+justinsteines@users.noreply.github.com>
2023-12-22 00:10:18 -0600

    feat: scroll to top on page change

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,15 +1,25 @@
-import { Outlet } from 'react-router-dom'
+import { useEffect, useRef } from 'react'
+import { Outlet, useLocation } from 'react-router-dom'
 
 import MainNavigation from '../components/MainNavigation'
 
 function MainLayout() {
+  const mainContentRef = useRef(null)
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    mainContentRef.current.scrollTo(0, 0)
+  }, [pathname])
+
   return (
-    <div className="grid grid-rows-[1fr_auto] h-screen-safe 2xl:container lg:grid-cols-[auto_1fr] lg:grid-rows-1 2xl:mx-auto">
-      <main className="overflow-auto">
-        <Outlet />
-      </main>
-      <MainNavigation />
-    </div>
+    <>
+      <div className="grid grid-rows-[1fr_auto] h-screen-safe 2xl:container lg:grid-cols-[auto_1fr] lg:grid-rows-1 2xl:mx-auto">
+        <main ref={mainContentRef} className="overflow-auto">
+          <Outlet />
+        </main>
+        <MainNavigation />
+      </div>
+    </>
   )
 }
 
